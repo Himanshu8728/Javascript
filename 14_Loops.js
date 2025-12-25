76024e71be06fc1e3d949301dfcf49dfd6044450
Himanshu8728 <himanshumourya2k3@gmail.com>
2025-12-25 21:05:58 +0530

    loops

---
+++ 14_Loops.js
@@ -0,0 +1,34 @@
+// for(let i = 1; i<=10;i++){
+//     console.log(i)
+// }
+
+// for(let i = 1;i<=10;i++){
+//     for(let j = 1;j<=10;j++){
+//         console.log(`${i} * ${j} =${i*j}`)
+//     }
+// }
+
+const myArray = ['flash', 'superman', 'captain america', 'ironman'];
+let j = 0;
+while(j<=myArray.length){
+    console.log(`SuperHero is ${myArray[j]}`)
+    j++;
+}
+
+// for(let i = 0;i<myArray.length;i++){
+//     console.log(myArray[i])
+// }
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index===5){
+//         continue;
+//     }
+//     console.log(`The value of i is ${index}`);
+    
+// }
+
+// let i = 0;
+// while(i<=20){
+//     console.log(`the value of i is ${i}`)
+//     i=i+3;
+// }
