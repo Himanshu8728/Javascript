2650f7ff92341c98bb49fc75be66a7508239d3d6
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2025-12-31 20:03:13 +0530

    yooo

--- 05_string.js
+++
@@ -1,5 +0,0 @@
-let name = "himanshu";
-let age = 23;
-// console.log(name+age);
-//console.log(`My user name is ${name}${age}`);
-
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-let sum =0;
-// for(let i =0; i<=total.();i++){
-//     sum+=total[i];
-// }
