264585ad4256287730f2fd5cd4f5393d659fa3a3
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-01-06 09:09:55 +0530

    classes

--- Classes_OOPs/OOP.js
+++ Classes_OOPs/OOP.js
@@ -14,8 +14,9 @@ function User(username, loginCount, isLoggedIn){
     return this
 }
 //use new to create a new isntance so that the second user cannot overwrite the details of userone
-const userOne = new User("himanshu", 12, true)
+const userOne = new User("hims", 12, true)
 const userTwo = new User("Parul", 10, false)
 
 console.log(userOne)
 console.log(userTwo.username)
+console.log(userTwo)
