cb5343f6d5988a89e012f40db34e520b6dcb40e4
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-01-06 18:26:50 +0530

    object

---
+++ Classes_OOPs/Object.js
@@ -0,0 +1,34 @@
+function multiplyBy5(num){
+    return num*5;
+}
+
+//multiplyBy5.power = 2
+// console.log(multiplyBy5.power);
+// console.log(multiplyBy5(2));
+// console.log(multiplyBy5.prototype) 
+
+function CreateUser(username, score){
+    this.username = username;
+    this.score = score;
+    
+    //return this
+
+}
+
+
+CreateUser.prototype.increment=function(){
+    this.score++;
+}
+
+CreateUser.prototype.printMe=function(){
+    console.log(this.score)
+}
+
+const user1 = new CreateUser("hims", 999)
+const user2 = new CreateUser("OnePiece", 1054)
+
+user1.increment()
+user1.printMe()
+
+user2.increment()
+user2.printMe()
---
+++ Classes_OOPs/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log(multiplyBy5.power);
+// console.log(multiplyBy5(2));
+// console.log(multiplyBy5.prototype) 
